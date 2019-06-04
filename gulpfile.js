e33e4206078aa198715a884f2bd358ddcff5a49e
xiaolinzi <1832834795@qq.comm>
2019-06-04 20:33:23 +0800

    v1.3

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
     var gulp = require('gulp');
     var uglify = require('gulp-uglify');
     var rename = require('gulp-rename');
-    var htmlmin = require('gulp-htmlmin');
+    // var htmlmin = require('gulp-htmlmin');
     var cssmin = require('gulp-cssmin');
     var imagemin = require('gulp-imagemin');
     var concat = require('gulp-concat');
     var spritesmash = require('gulp-spritesmash');
     var spritesmith = require('gulp.spritesmith');
     var buffer = require('gulp-buffer');
-
+    var htmlmin1 = require('gulp-htmlmin')
     gulp.task('imgmin', () => {
         return gulp.src('./src/image/**')
             .pipe(imagemin())
@@ -22,7 +22,7 @@
     //     })
     // });
     gulp.task('jsmin', () => {
-        return gulp.src(['./src/js/*.js', '!src/js/*min.js'])
+        return gulp.src(['dist/js/*.js', '!src/js/*min.js'])
             .pipe(uglify())
             .pipe(rename(function(path) {
                 path.extname = ".min.js";
@@ -30,13 +30,29 @@
             .pipe(gulp.dest('dist/js'));
     })
 
-    gulp.task('htmlmin', () => {
+    // gulp.task('htmlmin', () => {
+    //     return gulp.src(['./src/html/*.html', '!src/html/*min.html'])
+    //         .pipe(htmlmin())
+    //         .pipe(rename(function(path) {
+    //             path.extname = ".min.html";
+    //         }))
+    //         .pipe(gulp.dest('dist/html'))
+    // });
+    gulp.task('htmlmin1', () => {
         return gulp.src(['./src/html/*.html', '!src/html/*min.html'])
-            .pipe(htmlmin())
+            .pipe(htmlmin1({ collapseWhitespace: true }))
             .pipe(rename(function(path) {
                 path.extname = ".min.html";
             }))
             .pipe(gulp.dest('dist/html'))
+    });
+    gulp.task('cssmin', () => {
+        return gulp.src(['./src/css/*.css', '!src/css/*min.css'])
+            .pipe(cssmin())
+            .pipe(rename(function(path) {
+                path.extname = ".min.css";
+            }))
+            .pipe(gulp.dest('dist/css'))
     });
 
     gulp.task('sprit', function() {
